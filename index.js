e6d02d66c342e911c996b978da07e7f97c5f8102
Viciot <vfuentesnoguera@gmail.com>
2021-11-22 12:46:54 +0100

    test-messages

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    // console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
     });
 });
 
